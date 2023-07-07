9072f1d4fb9ed4644f97c5213cd26a17b3bc4b16
andreas yanuar <andreasyanuarhanaekan@gmail.com>
2023-07-07 16:09:42 +0700

    Second commit. Flex Wrap adjustment, JS routing optimization

--- app.js
+++ app.js
@@ -4,31 +4,24 @@ const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 const path = require('path')
 
-// const myModal = document.getElementById('myModal')
-// const myInput = document.getElementById('myInput')
-
-// myModal.addEventListener('shown.bs.modal', () => {
-//   myInput.focus()
-// })
-
 const app = express();
 const mongoose = require('mongoose');
 var _ = require('lodash');
 
-main().catch(err => console.log(err));
+// main().catch(err => console.log(err));
 
-async function main() {
-  await mongoose.connect('mongodb+srv://admin-rea:Test123@cluster0.a0g4oqq.mongodb.net/todolistDB', {useNewUrlParser: true});
-};
+// async function main() {
+//   await mongoose.connect('mongodb+srv://admin-rea:Test123@cluster0.a0g4oqq.mongodb.net/todolistDB', {useNewUrlParser: true});
+// };
 
-const itemsSchema = new mongoose.Schema({
-  name: {
-      type: String,
-      required: [true, "Please double check your data entry, no name specified"]
-  }
-});
+// const itemsSchema = new mongoose.Schema({
+//   name: {
+//       type: String,
+//       required: [true, "Please double check your data entry, no name specified"]
+//   }
+// });
 
-const Item = mongoose.model('Item', itemsSchema);
+// const Item = mongoose.model('Item', itemsSchema);
 
 app.set('view engine', 'ejs');
 
@@ -36,28 +29,40 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 // app.use("/styles/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")));
 
-const items = [];
-const workItems = [];
-
-const item1 = new Item({
-  name: "Eat"
-})
-
-const item2 = new Item({
-  name: "Sleep"
-})
-
-const item3 = new Item({
-  name: "Code"
-})
-
-const defaultItems = [item1, item2, item3];
-
-const customSchema = new mongoose.Schema({
-  name: String,
-  items: [itemsSchema]
+app.get('/', (req, res) => {
+  res.render("landingpage");
 });
-const List = mongoose.model('List', customSchema);
+
+app.get('/potentialproduct', (req, res) => {
+  res.render("potentialproduct");
+});
+
+app.get('/airlineperspective', (req, res) => {
+  res.render("airlineperspective");
+});
+
+// const items = [];
+// const workItems = [];
+
+// const item1 = new Item({
+//   name: "Eat"
+// })
+
+// const item2 = new Item({
+//   name: "Sleep"
+// })
+
+// const item3 = new Item({
+//   name: "Code"
+// })
+
+// const defaultItems = [item1, item2, item3];
+
+// const customSchema = new mongoose.Schema({
+//   name: String,
+//   items: [itemsSchema]
+// });
+// const List = mongoose.model('List', customSchema);
 
 // Get A Page with a param
 
@@ -90,58 +95,58 @@ const List = mongoose.model('List', customSchema);
 //   });
 // })
 
-app.get("/", function(req, res) {
+// app.get("/", function(req, res) {
 
-Item.find({})
-.then(function(foundItems){
-  if(foundItems.length === 0){
-    Item.insertMany(defaultItems)
-    .then(function(){
-      console.log("Succesfully added new items");
-    })
-    .catch(function(err){
-      console.log(err);
-    });
-    console.log(foundItems);
-    res.redirect("/");
-  } else {
-    const day = date.getDate();
+// Item.find({})
+// .then(function(foundItems){
+//   if(foundItems.length === 0){
+//     Item.insertMany(defaultItems)
+//     .then(function(){
+//       console.log("Succesfully added new items");
+//     })
+//     .catch(function(err){
+//       console.log(err);
+//     });
+//     console.log(foundItems);
+//     res.redirect("/");
+//   } else {
+//     const day = date.getDate();
 
-    res.render("landingpage");
-    // res.render("list", {listTitle: day, newListItems: foundItems, foo: "Sample"});
-  }
+//     res.render("landingpage");
+//     // res.render("list", {listTitle: day, newListItems: foundItems, foo: "Sample"});
+//   }
   
-})
-.catch(function(err){
-  console.log(err);
-});
+// })
+// .catch(function(err){
+//   console.log(err);
+// });
 
-});
+// });
 
-app.post("/", function(req, res){
+// app.post("/", function(req, res){
 
-  const itemName = req.body.newItem;
-  const listName = req.body.listButton;
+//   const itemName = req.body.newItem;
+//   const listName = req.body.listButton;
 
-  const item = new Item({
-    name: itemName
-  });
+//   const item = new Item({
+//     name: itemName
+//   });
 
-  if (listName === "Sample"){
-    item.save();
-    res.redirect("/");
-  } else {
-    List.findOne({name: listName})
-    .then(function(foundList){
-      foundList.items.push(item);
-      foundList.save();
-      res.redirect("/" + listName);
-    })
-  .catch(function(err){
-    console.log(err);
-    });
+//   if (listName === "Sample"){
+//     item.save();
+//     res.redirect("/");
+//   } else {
+//     List.findOne({name: listName})
+//     .then(function(foundList){
+//       foundList.items.push(item);
+//       foundList.save();
+//       res.redirect("/" + listName);
+//     })
+//   .catch(function(err){
+//     console.log(err);
+//     });
 
-  }
+//   }
 
   
 
@@ -152,97 +157,97 @@ app.post("/", function(req, res){
   //   items.push(item);
   //   res.redirect("/");
   // }
-});
+// });
 
 
-app.post("/delete", function(req, res){
-  const itemID = req.body.checkBox;
-  const listName = req.body.listInvisible;
+// app.post("/delete", function(req, res){
+//   const itemID = req.body.checkBox;
+//   const listName = req.body.listInvisible;
 
-  if (listName === "Sample"){
-    Item.findByIdAndRemove(itemID)
-    .then(function(){
-    console.log("Succesfully removed one items");
-    res.redirect("/");
-  })
-    .catch(function(err){
-    console.log(err);
-})
-  } else{
-    List.findOneAndUpdate({name: listName}, {$pull:{items:{_id: itemID}}})
-    .then(function(foundList){
-      res.redirect("/" + listName);
-    })
-    .catch(function(err){
-    console.log(err);
-    })
-  }
+//   if (listName === "Sample"){
+//     Item.findByIdAndRemove(itemID)
+//     .then(function(){
+//     console.log("Succesfully removed one items");
+//     res.redirect("/");
+//   })
+//     .catch(function(err){
+//     console.log(err);
+// })
+//   } else{
+//     List.findOneAndUpdate({name: listName}, {$pull:{items:{_id: itemID}}})
+//     .then(function(foundList){
+//       res.redirect("/" + listName);
+//     })
+//     .catch(function(err){
+//     console.log(err);
+//     })
+//   }
   
-})
+// })
 
 // app.get("/work", function(req,res){
 //   res.render("list", {listTitle: "Work List", newListItems: workItems});
 // });
 
-app.get("/potentialproduct", function(req, res) {
+// app.get("/potentialproduct", function(req, res) {
 
-  Item.find({})
-  .then(function(foundItems){
-    if(foundItems.length === 0){
-      Item.insertMany(defaultItems)
-      .then(function(){
-        console.log("Succesfully added new items");
-      })
-      .catch(function(err){
-        console.log(err);
-      });
-      console.log(foundItems);
-      res.redirect("/");
-    } else {
-      const day = date.getDate();
+//   Item.find({})
+//   .then(function(foundItems){
+//     if(foundItems.length === 0){
+//       Item.insertMany(defaultItems)
+//       .then(function(){
+//         console.log("Succesfully added new items");
+//       })
+//       .catch(function(err){
+//         console.log(err);
+//       });
+//       console.log(foundItems);
+//       res.redirect("/");
+//     } else {
+//       const day = date.getDate();
   
-      res.render("potentialproduct");
+//       res.render("potentialproduct");
+//       // res.render("list", {listTitle: day, newListItems: foundItems, foo: "Sample"});
+//     }
+    
+//   })
+//   .catch(function(err){
+//     console.log(err);
+//   });
+  
+//   });
+
+// app.get("/airlineperspective", function(req, res) {
+
+//   Item.find({})
+//   .then(function(foundItems){
+//     if(foundItems.length === 0){
+//       Item.insertMany(defaultItems)
+//       .then(function(){
+//         console.log("Succesfully added new items");
+//       })
+//       .catch(function(err){
+//         console.log(err);
+//       });
+//       console.log(foundItems);
+//       res.redirect("/");
+//     } else {
+//       const day = date.getDate();
+  
+//       res.render("airlineperspective");
       // res.render("list", {listTitle: day, newListItems: foundItems, foo: "Sample"});
-    }
+  //   }
     
-  })
-  .catch(function(err){
-    console.log(err);
-  });
-  
-  });
-
-app.get("/airlineperspective", function(req, res) {
-
-  Item.find({})
-  .then(function(foundItems){
-    if(foundItems.length === 0){
-      Item.insertMany(defaultItems)
-      .then(function(){
-        console.log("Succesfully added new items");
-      })
-      .catch(function(err){
-        console.log(err);
-      });
-      console.log(foundItems);
-      res.redirect("/");
-    } else {
-      const day = date.getDate();
-  
-      res.render("airlineperspective");
-      // res.render("list", {listTitle: day, newListItems: foundItems, foo: "Sample"});
-    }
+  // })
+  // .catch(function(err){
+  //   console.log(err);
+  // });
     
-  })
-  .catch(function(err){
-    console.log(err);
-  });
-    
-    });
+  //   });
 
-app.get("/about", function(req, res){
-  res.render("about");
-});
+// app.get("/about", function(req, res){
+//   res.render("about");
+// });
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
